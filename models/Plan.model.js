7fdb60611e4ded67cf62d9a32b265c438510b77b
Yabel <yabelrr@gmail.com>
2022-11-02 17:00:15 +0100

    User and Plan model created

---
+++ models/Plan.model.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const planSchema = new Schema(
+  {
+    title : {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image : {
+        type: String,
+        default: "https://picsum.photos/300"
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    tags: [{type: String}],
+
+    // Arrays Party Fiesta
+    attendees: [{type: Schema.Types.ObjectId, ref: "User"}]
+
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Plan = model("Plan", planSchema);
+
+module.exports = Plan;
--- models/User.model.js
+++ models/User.model.js
@@ -18,6 +18,38 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Name is required."],
     },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    birthdate: {
+      type: String
+    },
+    gender: {
+      enum: ["male", "female", "other"]
+    },
+    profileImage: {
+      type: String,
+      default: "https://picsum.photos/300"
+    },
+    phoneNumber: {
+      type: String
+    },
+    country: {
+      type: String
+    },
+
+    // Arrays Party Fiesta
+
+    //Friends
+    friends: [{type: Schema.Types.ObjectId, ref: "User"}],
+    friendsRequested: [{type: Schema.Types.ObjectId, ref: "User"}],
+    friendsToAccept: [{type: Schema.Types.ObjectId, ref: "User"}],
+
+    //Plans
+    plans: [{type: Schema.Types.ObjectId, ref: "Plan", status: {enum: ["confirmed", "declined", "pending", "admin"]}}]
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
